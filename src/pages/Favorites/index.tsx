2db5bb483e43137441cb72efed2bf02f01fa448b
AndreVTavares <andretavares3@gmail.com>
2020-08-27 12:07:55 -0300

    Listing favorites on the favorites page

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, ScrollView } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 import PageHeader from '../../components/PageHeader';
 
 import styles from './styles';
 
 const Favorites = () => {
+  const [favorites, setFavorites] = useState([]);
+
+  function loadFavorites() {
+    AsyncStorage.getItem('favorites').then((response) => {
+      if (response) {
+        const favoritedTeachers = JSON.parse(response);
+
+        setFavorites(favoritedTeachers);
+      }
+    });
+  }
+
+  useEffect(() => {
+    loadFavorites();
+  });
+
   return (
     <View style={styles.container}>
       <PageHeader title="Meus proffys favoritos" />
@@ -18,7 +35,9 @@ const Favorites = () => {
           paddingBottom: 16,
         }}
       >
-       
+        {favorites.map((favorite: Teacher) => (
+          <TeacherItem key={favorite.id} teacher={favorite} favorited />
+        ))}
       </ScrollView>
     </View>
   );
